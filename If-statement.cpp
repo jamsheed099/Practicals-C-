6b158a4c8045fcb28e9a31a516313279616583df
jamsheed099 <113326012+jamsheed099@users.noreply.github.com>
2023-03-31 11:10:57 +0500

    Create If-statement.cpp

---
+++ If-statement.cpp
@@ -0,0 +1,20 @@
+Practical: 12.1.2
+Code:
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    cout<<"*** IF STATEMENT***"<<endl;
+    int a = 12;
+    
+    if(a < 20){
+        cout<<" a is less 20:"<<endl;
+    }
+    cout<<"Value of a is :"<< a <<endl;
+    return 0;
+}
+
+
+
